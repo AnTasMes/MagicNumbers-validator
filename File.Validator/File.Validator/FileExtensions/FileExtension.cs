0cdd05a71c918c023222ea1c242fac06f61c1317
Andrej Tasevski Mestrovic <andrejmtasevski@gmail.com>
2024-08-10 12:23:01 +0200

    Refactored some code for later use

--- File.Validator/File.Validator/FileExtensions/FileExtension.cs
+++ File.Validator/File.Validator/FileExtensions/FileExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace File.Validator.FileExtensions
 {
@@ -9,5 +10,23 @@ namespace File.Validator.FileExtensions
         public abstract string Name { get; }
         public abstract int Offset { get; }
         public abstract string[] Hex { get; }
+
+        public virtual bool IsValid (BinaryReader reader)
+        {
+            // Postavi offset
+            reader.BaseStream.Seek(this.Offset, SeekOrigin.Begin);
+
+            // Izvlaci broj bajtova u najduzem hex potpisu
+            string longestSignature = this.Hex.OrderByDescending(h => h.Length).First();
+            int longestSignatureLen = Regex.Matches(longestSignature, "[a-zA-Z0-9][a-zA-Z0-9]").Count;
+
+            // Procitaj N bajtova iz niza
+            byte[] data = reader.ReadBytes(longestSignatureLen);
+            string data_as_hex = BitConverter.ToString(data);
+            reader.Close();
+
+            // Proveriti da li data_as_hex poseduje neki do potpisa u nizu
+            return this.Hex.Any(h => data_as_hex.Contains(h));
+        }
     }
 }
--- File.Validator/File.Validator/FileValidator.cs
+++ File.Validator/File.Validator/FileValidator.cs
@@ -1,11 +1,5 @@
-﻿using File.Validator;
-using File.Validator.FileExtensions;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using File.Validator.FileExtensions;
 using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace File.Validator
 {
@@ -43,20 +37,7 @@ namespace File.Validator
             if (fileExtensionInfo == null)
                 return false;
 
-            // Postavi offset
-            reader.BaseStream.Seek(fileExtensionInfo.Offset, SeekOrigin.Begin);
-
-            // Izvlaci broj bajtova u najduzem hex potpisu
-            string longestSignature = fileExtensionInfo.Hex.OrderByDescending(h => h.Length).First();
-            int longestSignatureLen = Regex.Matches(longestSignature, "[a-zA-Z0-9][a-zA-Z0-9]").Count;
-
-            // Procitaj N bajtova iz niza
-            byte[] data = reader.ReadBytes(longestSignatureLen);   
-            string data_as_hex = BitConverter.ToString(data);
-            reader.Close();
-
-            // Proveriti da li data_as_hex poseduje neki do potpisa u nizu
-            return fileExtensionInfo.Hex.Any(h => data_as_hex.Contains(h));
+            return fileExtensionInfo.IsValid(reader);
         }
 
         /// <summary>
